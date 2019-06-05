c675903caa819faa381b9e03cd0be2878b893e75
adam somora <adamsomora33@gmail.com>
2019-06-05 23:08:34 +0200

    Rails Simple Airbnb exercise - done until the 5 of 10 points, so alsmot half done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+5.times do
+  Flat.create!(
+    name: Faker::Superhero.name,
+    address: Faker::Address.full_address,
+    description: Faker::Quote.famous_last_words,
+    price_per_night: rand(50..100),
+    number_of_guests: rand(1..5)
+  )
+end
